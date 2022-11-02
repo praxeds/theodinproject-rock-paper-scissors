4074e462676d456c11e1e3f762442fa98c90bf6f
Juliana Praxedes <julianapraxeds@gmail.com>
2022-11-02 14:26:38 -0300

    added game functionalities

--- script.js
+++ script.js
@@ -1,11 +1,58 @@
-const gameOptions = ['Rock', 'Paper', 'Scissors']
+const gameOptions = [
+    {
+        'type': 'rock',
+        'losesTo': 'paper'
+    },
+    {
+        'type': 'paper',
+        'losesTo': 'scissors'
+    },
+    {
+        'type': 'scissors',
+        'losesTo': 'rock'
+    }
+]
 
-function getComputerChoice(arr) {
+//!Buttons Specificities
+var playerSelection
+document.getElementById('rock').addEventListener('click', function(){
+    playerSelection = gameOptions[0]
+    console.log(playerSelection.type)
+})
 
-    const randomIndex = Math.floor(Math.random() * arr.length)
+document.getElementById('paper').addEventListener('click', function(){
+    playerSelection = gameOptions[1]
+    console.log(playerSelection.type)
+})
 
-    const item = arr[randomIndex]
+document.getElementById('scissors').addEventListener('click', function(){
+    playerSelection = gameOptions[2]
+    console.log(playerSelection.type)
+})
 
-    return item
+//!Starting a round
+var computerSelection
+const gameButtons = document.querySelectorAll('button')
+
+function getComputerChoice() {
+    const randomIndex = Math.floor(Math.random() * gameOptions.length)
+    computerSelection = gameOptions[randomIndex].type
+    console.log(computerSelection)
 }
+
+for (let i = 0; i < gameButtons.length; i++) {
+    let button = gameButtons[i]
+
+    button.addEventListener('click', function() {
+        getComputerChoice()
+
+        if (playerSelection.type === computerSelection) {
+            console.log('%cdraw huhu', 'color: dodgerblue; font-size: 50px')
+        } else if (playerSelection.losesTo === computerSelection) {
+            console.log('%cyou lose :(', 'color: red; font-size: 50px; font-weight: bold')
+        } else {
+            console.log('%cyou win! whoop whoop', 'color: green; font-size: 50px; font-weight: bold')
+        }
+    })
+  }
 
