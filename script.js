26813b2e2a0970a0fcfb8929b5fcc12f0933c6c6
Juliana Praxedes <julianapraxeds@gmail.com>
2022-11-03 16:23:45 -0300

    overall css done, more js features to come

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const gameOptions = [
     {
         'type': 'rock',
-        'losesTo': 'paper'
+        'losesTo': 'paper',
+        'emoji' : '🪨'
     },
     {
         'type': 'paper',
-        'losesTo': 'scissors'
+        'losesTo': 'scissors',
+        'emoji' : '📄'
     },
     {
         'type': 'scissors',
-        'losesTo': 'rock'
+        'losesTo': 'rock',
+        'emoji' : '✂️'
     }
 ]
 
@@ -17,27 +20,32 @@ const gameOptions = [
 var playerSelection
 document.getElementById('rock').addEventListener('click', function(){
     playerSelection = gameOptions[0]
-    console.log(playerSelection.type)
+    console.log('You chose: ' + playerSelection.type + playerSelection.emoji)
 })
 
 document.getElementById('paper').addEventListener('click', function(){
     playerSelection = gameOptions[1]
-    console.log(playerSelection.type)
+    console.log('You chose: ' + playerSelection.type + playerSelection.emoji)
 })
 
 document.getElementById('scissors').addEventListener('click', function(){
     playerSelection = gameOptions[2]
-    console.log(playerSelection.type)
+    console.log('You chose: ' + playerSelection.type + playerSelection.emoji)
 })
 
 //!Starting a round
 var computerSelection
-const gameButtons = document.querySelectorAll('button')
+const gameButtons = document.querySelectorAll('.gameButton')
 
 function getComputerChoice() {
     const randomIndex = Math.floor(Math.random() * gameOptions.length)
     computerSelection = gameOptions[randomIndex].type
-    console.log(computerSelection)
+    const consoleEmoji = gameOptions[randomIndex].emoji
+    console.log('Computer chose: ' + computerSelection + consoleEmoji)
+}
+
+function disappearSpan() {
+    document.getElementById('chooseOneSpan').classList.add('noDisplay')
 }
 
 for (let i = 0; i < gameButtons.length; i++) {
@@ -45,13 +53,14 @@ for (let i = 0; i < gameButtons.length; i++) {
 
     button.addEventListener('click', function() {
         getComputerChoice()
+        disappearSpan()
 
         if (playerSelection.type === computerSelection) {
-            console.log('%cdraw huhu', 'color: dodgerblue; font-size: 50px')
+            console.log('%cdraw huhu',  'color: #4399dd; font-size: 25px')
         } else if (playerSelection.losesTo === computerSelection) {
-            console.log('%cyou lose :(', 'color: red; font-size: 50px; font-weight: bold')
+            console.log('%cyou lose :(', 'color: #e44535; font-size: 25px; font-weight: bold')
         } else {
-            console.log('%cyou win! whoop whoop', 'color: green; font-size: 50px; font-weight: bold')
+            console.log('%cyou win! whoop whoop', 'color: #6eb179; font-size: 25px; font-weight: bold')
         }
     })
   }
