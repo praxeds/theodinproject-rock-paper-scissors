95a851326aa61f839cf2e0d8453ceda8197118d6
Juliana Praxedes <julianapraxeds@gmail.com>
2022-11-04 16:12:28 -0300

    More JS

--- script.js
+++ script.js
@@ -18,20 +18,36 @@ const gameOptions = [
 
 //!Buttons Specificities
 var playerSelection
-document.getElementById('rock').addEventListener('click', function () {
-    playerSelection = gameOptions[0]
-    console.log('You chose: ' + playerSelection.type + playerSelection.emoji)
-})
 
-document.getElementById('paper').addEventListener('click', function () {
-    playerSelection = gameOptions[1]
-    console.log('You chose: ' + playerSelection.type + playerSelection.emoji)
-})
+const rockEl = document.querySelectorAll('.rock')
+for (let i = 0; i < rockEl.length; i++) {
+    let button = rockEl[i]
+    button.addEventListener('click', function () {
+        playerSelection = gameOptions[0]
+        console.log('You chose: ' + playerSelection.type + playerSelection.emoji)
+    })
+}
 
-document.getElementById('scissors').addEventListener('click', function () {
-    playerSelection = gameOptions[2]
-    console.log('You chose: ' + playerSelection.type + playerSelection.emoji)
-})
+const paperEl = document.querySelectorAll('.paper')
+for (let i = 0; i < paperEl.length; i++) {
+    let button = paperEl[i]
+    button.addEventListener('click', function () {
+        playerSelection = gameOptions[1]
+        console.log('You chose: ' + playerSelection.type + playerSelection.emoji)
+    })
+}
+
+
+const scissorsEl = document.querySelectorAll('.scissors')
+for (let i = 0; i < scissorsEl.length; i++) {
+    let button = scissorsEl[i]
+    button.addEventListener('click', function () {
+        playerSelection = gameOptions[2]
+        console.log('You chose: ' + playerSelection.type + playerSelection.emoji)
+    })
+}
+
+
 
 
 //!Starting a round
@@ -71,10 +87,10 @@ for (let i = 0; i < gameButtons.length; i++) {
             console.log('%cdraw huhu', 'color: #4399dd; font-size: 25px')
         } else if (playerSelection.losesTo === computerSelection) {
             console.log('%cyou lose 😔', 'color: #e44535; font-size: 25px; font-weight: bold')
-            
+
             computerScore += 1
             computerScoreEl.innerText = computerScore
-            
+
         } else {
             console.log('%cyou win! whoop whoop', 'color: #6eb179; font-size: 25px; font-weight: bold')
             userScore += 1
@@ -82,4 +98,3 @@ for (let i = 0; i < gameButtons.length; i++) {
         }
     })
 }
-
